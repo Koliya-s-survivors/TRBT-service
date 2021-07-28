03d19b33d24af5d817c1266766788ec0d0e18a39
rzachepa <telp.r.zachepa@gmail.com>
2021-07-29 01:50:18 +0300

    create catalog-content-item component

--- src/commons/components/pages/catalogue/catalogue.js
+++ src/commons/components/pages/catalogue/catalogue.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import CatalogContentItem from "./catalog-content-item/catalog-content-item";
 
 export default class Catalogue extends Component {
   render() {
-    return <section> This is Catalogue section</section>
+    return (
+    <>
+      <section> This is Catalogue section</section>
+      <CatalogContentItem/>
+    </>
+    )
   }
 }
