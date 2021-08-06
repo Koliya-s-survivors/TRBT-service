54006b0e613b417cc36368fca6cc5a4f7a80f808
Eugen-Ivanushkin <eugen1mn@gmail.com>
2021-08-06 22:53:53 +0300

    update log-in-form component with use TypeScript

--- src/commons/components/log-in-form/log-in-form.tsx
+++ src/commons/components/log-in-form/log-in-form.tsx
@@ -3,16 +3,18 @@ import React, { Component } from "react";
 // styles
 import './log-in-form.css'
 
-export default class LogInForm extends Component {
+export default class LogInForm extends Component<{}, {}> {
     render() {
         return <article className="log-in-form">
             <section >
                 <h3>Authorize</h3>
                 <form>
+                    {/* @ts-ignore */}
                     <label for="user_email">E-mail:</label>
                     <input name="email" type="text" id="user_email" />
                     <p></p>
-
+                    
+                    {/* @ts-ignore */}
                     <label for="user_password">Password:</label>
                     <input name="password" type="password" id="user_password" />
                     <p></p>
