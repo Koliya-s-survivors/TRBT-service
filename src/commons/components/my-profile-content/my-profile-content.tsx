6c962f6902c9c35da545dcf7de81520e2f52506b
Apxucmpax <apxucmpax@mail.ru>
2021-08-07 19:16:46 +0300

    update my-profile-content component with use TypeScript

---
+++ src/commons/components/my-profile-content/my-profile-content.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import MyProfileContentItem from '../my-profile-content-item';
+
+// CSS
+import './my-profile-content.css';
+
+export interface MyProfileContentProps {
+    sectionName: string,
+    itemArray: any[] //item description required
+}
+
+const MyProfileContent: React.FunctionComponent<MyProfileContentProps> = ({ sectionName, itemArray}) => {
+    return (
+        <article className="MyProfileContent">
+            <section>
+                <h2>
+                    {sectionName}
+                </h2>
+            </section>
+            <section>
+                <ul>
+                    {itemArray.map(function (item) {
+                        return <MyProfileContentItem key={item.id} melody={item}/>
+                    })}
+                </ul>
+            </section>
+        </article>
+    )
+}
+
+export default MyProfileContent;
