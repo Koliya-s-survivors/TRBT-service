07a31fc3876bfdd77435c25610172bfe8b22e09d
Elena Solomakha <telp.e.solomakha@gmail.com>
2021-08-10 21:13:50 -0700

    fix catalod-categories-item component

--- src/commons/components/catalog-categories-item/catalog-categories-item.js
+++ src/commons/components/catalog-categories-item/catalog-categories-item.js
@@ -3,10 +3,10 @@ import React from 'react';
 // style
 import './catalog-categories-item.css';
 
-const CatalogCategoriesItem = (props) => {
+const CatalogCategoriesItem = ({isActiveCategory, category, categoryClick}) => {
   return(
     <li className='catalog-categories-item'>
-      <button className={props.isActiveCategory === props.category ? 'catalog-categories-item-active' : null} onClick={(e) => props.categoryClick(e.target.innerText)}>{props.category}</button>
+      <button className={isActiveCategory === category ? 'catalog-categories-item-active' : null} onClick={() => categoryClick(category)}>{category}</button>
     </li>
   );
 };
