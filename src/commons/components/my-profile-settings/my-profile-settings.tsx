cb39986991289df6635faad315609acb456acae2
Eugen-Ivanushkin <eugen1mn@gmail.com>
2021-08-12 23:14:35 +0300

    Create my-profile-settings component

---
+++ src/commons/components/my-profile-settings/my-profile-settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+ const MyProfileSettings:React.FC<Props> = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default MyProfileSettings
