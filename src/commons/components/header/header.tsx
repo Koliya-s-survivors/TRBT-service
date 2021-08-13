b670f9615ef145a491ba60e521c9ff325533f2fd
rzachepa <telp.r.zachepa@gmail.com>
2021-08-13 21:39:02 +0300

    update header component

--- src/commons/components/header/header.tsx
+++ src/commons/components/header/header.tsx
@@ -7,7 +7,7 @@ import HeaderNav from '../header-nav';
 // styles
 import './header.css';
 
-const Header = () => {
+const Header:React.FC = () => {
   return (
     <header className="header">
       <HeaderLogo/>
