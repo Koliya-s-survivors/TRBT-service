85f209712af1b755dfaafcca1a927d08adf561bc
Eugen-Ivanushkin <eugen1mn@gmail.com>
2021-08-14 12:52:51 +0300

    fix some bugs

--- src/commons/components/my-profile-settings/my-profile-settings.tsx
+++ src/commons/components/my-profile-settings/my-profile-settings.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+//components
+import MyProfileSettingsPasswordChange from '../my-profile-settings-password-change'
+
 interface Props {}
 
 const MyProfileSettings: React.FC<Props> = () => {
     return (
-        <>
-            
-        </>
+        <MyProfileSettingsPasswordChange/>
     )
 };
 
