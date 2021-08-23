d5ba7812be7a02942ad9a19616fdbbe785e3f41b
rzachepa <telp.r.zachepa@gmail.com>
2021-08-23 16:28:14 +0300

    refactor header-logo component

--- src/commons/components/header-logo/header-logo-styles.ts
+++ src/commons/components/header-logo/header-logo-styles.ts
@@ -1,6 +1,6 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   headerLogo: {
     fontSize: '2.5rem',
     fontWeight: 'bold',
