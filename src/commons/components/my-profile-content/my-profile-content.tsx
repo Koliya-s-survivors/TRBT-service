bff47d0fc95ea42bbe2bb1ab487d54df1ace8846
rzachepa <telp.r.zachepa@gmail.com>
2021-09-02 22:47:55 +0300

    fix some bugs

--- src/commons/components/my-profile-content/my-profile-content.tsx
+++ src/commons/components/my-profile-content/my-profile-content.tsx
@@ -7,34 +7,34 @@ import MyProfileContentItem from '../my-profile-content-item';
 import { Typography, Box, Grid } from '@material-ui/core';
 
 // style
- 
+
 
 // interface
- interface MyProfileContentProps {
+interface MyProfileContentProps {
   sectionName: string,
   itemArray: any[] // item description required
 }
 
 
 const MyProfileContent: React.FC<MyProfileContentProps> = ({ itemArray }) => {
-  
+
   return (
     <Box p={1.25}>
-        <Grid container >
-          <Grid item xs={12}>
-            <Typography variant="h4">
-              Content
-            </Typography>
-          </Grid>
-          <Grid item xs={12} lg={6}>
-            {itemArray.map(item =>
-              <MyProfileContentItem key={item.key}
-                melody={item.melody}
-              />
-            )}
-          </Grid>
+      <Grid container >
+        <Grid item xs={12}>
+          <Typography variant="h4">
+            Content
+          </Typography>
         </Grid>
-      </Box>  
+        <Grid item xs={12} >
+          {itemArray.map(item =>
+            <MyProfileContentItem key={item.key}
+              melody={item.melody}
+            />
+          )}
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
