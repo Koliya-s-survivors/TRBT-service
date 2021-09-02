9805c0d2ae6ec99f4d79f1eb5b4fc2152da395eb
rzachepa <telp.r.zachepa@gmail.com>
2021-09-02 23:50:27 +0300

    create my-profile-history component with use TS and material UI

--- src/commons/components/pages/my-profile/my-profile.js
+++ src/commons/components/pages/my-profile/my-profile.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 // components
 import MyProfileInfoPanel from '../../my-profile-info-panel';
 // import MyProfileContent from '../../my-profile-content';
+// import MyProfileHistory from '../../my-profile-history';
 // import itemArray from '../../../api/my-profile/my-profile-content';
 
 export default class MyProfile extends Component {
@@ -11,6 +12,7 @@ export default class MyProfile extends Component {
       <>
         <MyProfileInfoPanel />
         {/* <MyProfileContent itemArray={itemArray}/> */}
+        {/* <MyProfileHistory itemArray={itemArray} isHistoryComponent={true}/> */}
       </>
     )
   }
