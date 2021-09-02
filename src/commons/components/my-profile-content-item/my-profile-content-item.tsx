bf14b9a1a71c1dd5565400dbb551723a970afc4d
rzachepa <telp.r.zachepa@gmail.com>
2021-09-02 03:15:57 +0300

    create styles file,create className and add styles

--- src/commons/components/my-profile-content-item/my-profile-content-item.tsx
+++ src/commons/components/my-profile-content-item/my-profile-content-item.tsx
@@ -8,8 +8,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
 
 // styles
-import './my-profile-content-item.css'
-import './my-profile-content-item-media.css'
+import useStyles from './my-profile-content-item-styles';
 
 
 interface MyProfileContentItemProps {
@@ -26,6 +25,7 @@ interface Melody {
 }
 
 const MyProfileContentItem: React.FC<MyProfileContentItemProps> = (props) => {
+  const myProfileContentItemClasses : any = useStyles();
   // const {  
   //   musicName = 'Тема усатого нян',
   //   artist = 'А. Рыбников',
@@ -34,66 +34,140 @@ const MyProfileContentItem: React.FC<MyProfileContentItemProps> = (props) => {
 
   return (
     <Box p={1.25}>
-      <Grid container >
-        <Grid container item xs={7} style={{ maxWidth: '800px', border: '3px solid #64DD17', borderRadius: '4px', padding: '15px', display: 'flex', flexDirection: 'row' }}>
-          <Grid item xs={12} md={3}>
-            <Card style={{ boxShadow: 'none', borderRadius: 'none', minWidth: '170px', maxWidth: '185px', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '15px', backgroundColor: '#e3e3e3' }}>
-              <CardMedia
-                image=' https://source.unsplash.com/random'
-                title='Image title'
-                style={{ width: '160px', height: '160px', borderRadius: '10px', justifySelf: 'center' }}
-              />
+      <Grid container xs={12} sm={12} md={8} /* поставить при медиа запросе 320px justifyContent={'center'}*/ className={myProfileContentItemClasses.firstGridContainer}>
+        <Card className={myProfileContentItemClasses.card} >
+          <CardMedia
+            image=' https://source.unsplash.com/random'
+            title='Image title'
+             className={myProfileContentItemClasses.cardMedia}
+          // style={{ width: '160px', height: '160px', borderRadius: '10px', justifySelf: 'center' }}
+          />
 
-              <CardContent>
-                <Grid container >
-                  <Grid item xs={12}>
-                    <Typography >
-                      Тема усатого нян
-                    </Typography>
-                  </Grid>
-                  <Grid container item xs={12}>
-                    <Grid item xs={10}>
-                      <Typography style={{ color: '#1774A4' }}>
-                        А. Рыбников
-                      </Typography>
-                    </Grid>
-                    <Grid item xs>
-                      <IconButton aria-label="info" style={{ padding: '0' }}>
-                        <Link to='/' >
-                          <InfoOutlinedIcon style={{ color: 'black' }} />
-                        </Link>
-                      </IconButton>
-                    </Grid>
+          <CardContent>
 
+            <Grid container >
+              <Grid item xs={12}>
+                <Typography >
+                  Тема усатого нян
+                </Typography>
+              </Grid>
 
-                  </Grid>
+              <Grid container item xs={12}>
+                <Grid item xs={10}>
+                  <Typography className={myProfileContentItemClasses.artist} >
+                    А. Рыбников
+                  </Typography>
                 </Grid>
-              </CardContent>
-            </Card>
 
+                <Grid item xs>
+                  <IconButton aria-label="info" className={myProfileContentItemClasses.iconBtn} >
+                    <Link to='/' >
+                      <InfoOutlinedIcon className={myProfileContentItemClasses.icon} />
+                    </Link>
+                  </IconButton>
+                </Grid>
+
+              </Grid>
+            </Grid>
+
+          </CardContent>
+
+        </Card>
+
+        <Grid container item xs={8} sm={6} md={6} className={myProfileContentItemClasses.secondGridContainer}>
+
+          <Grid item xs={12} className={myProfileContentItemClasses.description}>
+            <Typography>
+              Date of purchase: 2017-06-22 10:33:14
+            </Typography>
+            <Typography>
+              Paid period of validity: 2017-07-02 00:00:00
+            </Typography>
           </Grid>
-          <Grid container item xs style={{ padding: '15px 10px' }}>
-            <Grid item xs={12} style={{ marginBottom: '5px' }}>
-              <Typography>
-                Date of purchase: 2017-06-22 10:33:14
-              </Typography>
-              <Typography>
-                Paid period of validity: 2017-07-02 00:00:00
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} style={{ color: '#64DD17', padding: '0' }} />default play
-            </Grid>
-            <Grid item xs={12} style={{ margin: '5px 0' }}>
-              <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} style={{ color: '#64DD17', padding: '0' }} />default play
-            </Grid>
-            <Grid item xs={12}>
-              <Button variant="contained" style={{ backgroundColor: '#fff', color: '#64DD17', border: '1px solid #64DD17', boxShadow: 'none' }}>Default</Button>
-            </Grid>
+
+          <Grid item xs={12} >
+            <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} className={myProfileContentItemClasses.checkbox} />default play
           </Grid>
+
+          <Grid item xs={12} style={{ margin: '5px 0' }}>
+            <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} className={myProfileContentItemClasses.checkbox} />default play
+          </Grid>
+
+          <Grid item xs={12} >
+            <Button variant="contained" className={myProfileContentItemClasses.button}>Default</Button>
+          </Grid>
+
         </Grid>
+
       </Grid>
-    </Box >
+    </Box>
+
+
+
+
+
+
+    // <Box p={1.25}>
+    //   <Grid container >
+    //     <Grid container item xs={7} style={{ maxWidth: '800px', border: '3px solid #64DD17', borderRadius: '4px', padding: '15px', display: 'flex', flexDirection: 'row' }}>
+    //       <Grid item xs={12} md={3}>
+    //         <Card style={{ boxShadow: 'none', borderRadius: 'none', minWidth: '170px', maxWidth: '185px', display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '15px', backgroundColor: '#e3e3e3' }}>
+    //           <CardMedia
+    //             image=' https://source.unsplash.com/random'
+    //             title='Image title'
+    //             style={{ width: '160px', height: '160px', borderRadius: '10px', justifySelf: 'center' }}
+    //           />
+
+    //           <CardContent>
+    //             <Grid container >
+    //               <Grid item xs={12}>
+    //                 <Typography >
+    //                   Тема усатого нян
+    //                 </Typography>
+    //               </Grid>
+    //               <Grid container item xs={12}>
+    //                 <Grid item xs={10}>
+    //                   <Typography style={{ color: '#1774A4' }}>
+    //                     А. Рыбников
+    //                   </Typography>
+    //                 </Grid>
+    //                 <Grid item xs>
+    //                   <IconButton aria-label="info" style={{ padding: '0' }}>
+    //                     <Link to='/' >
+    //                       <InfoOutlinedIcon style={{ color: 'black' }} />
+    //                     </Link>
+    //                   </IconButton>
+    //                 </Grid>
+
+
+    //               </Grid>
+    //             </Grid>
+    //           </CardContent>
+    //         </Card>
+
+    //       </Grid>
+    //       <Grid container item xs style={{ padding: '15px 10px' }}>
+    //         <Grid item xs={12} style={{ marginBottom: '5px' }}>
+    //           <Typography>
+    //             Date of purchase: 2017-06-22 10:33:14
+    //           </Typography>
+    //           <Typography>
+    //             Paid period of validity: 2017-07-02 00:00:00
+    //           </Typography>
+    //         </Grid>
+    //         <Grid item xs={12}>
+    //           <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} style={{ color: '#64DD17', padding: '0' }} />default play
+    //         </Grid>
+    //         <Grid item xs={12} style={{ margin: '5px 0' }}>
+    //           <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} style={{ color: '#64DD17', padding: '0' }} />default play
+    //         </Grid>
+    //         <Grid item xs={12}>
+    //           <Button variant="contained" style={{ backgroundColor: '#fff', color: '#64DD17', border: '1px solid #64DD17', boxShadow: 'none' }}>Default</Button>
+    //         </Grid>
+    //       </Grid>
+    //     </Grid>
+    //   </Grid>
+    // </Box >
   )
 };
 
