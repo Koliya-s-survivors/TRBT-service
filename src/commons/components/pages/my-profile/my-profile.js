a5feadb478a586510bb0ff93d2e792446cd581f0
rzachepa <telp.r.zachepa@gmail.com>
2021-09-04 00:13:58 +0300

    refactor my-profile-settings-passwotd-change with use material UI

--- src/commons/components/pages/my-profile/my-profile.js
+++ src/commons/components/pages/my-profile/my-profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 // components
 import MyProfileInfoPanel from '../../my-profile-info-panel';
+// import MyProfileSettings from '../../my-profile-settings';
 // import MyProfileContent from '../../my-profile-content';
 // import MyProfileHistory from '../../my-profile-history';
 // import itemArray from '../../../api/my-profile/my-profile-content';
@@ -11,6 +12,7 @@ export default class MyProfile extends Component {
     return (
       <>
         <MyProfileInfoPanel />
+        {/* <MyProfileSettings/> */}
         {/* <MyProfileContent itemArray={itemArray}/> */}
         {/* <MyProfileHistory itemArray={itemArray} isHistoryComponent={true}/> */}
       </>
