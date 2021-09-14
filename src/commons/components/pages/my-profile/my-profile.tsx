956541a9be54ebef2e06040c4428d445e252ce97
Salkov, Oleksii <osalkov@hq.telesens.lan>
2021-09-14 22:27:26 +0300

    change type files js to ts, tsx

---
+++ src/commons/components/pages/my-profile/my-profile.tsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Interface } from 'readline';
+
+// components
+import MyProfileInfoPanel from '../../my-profile-info-panel';
+import MyProfileTabNavigation from '../../my-profile-tab-navigation';
+// import MyProfileSettings from '../../my-profile-settings';
+// import MyProfileContent from '../../my-profile-content';
+// import MyProfileHistory from '../../my-profile-history';
+// import itemArray from '../../../api/my-profile/my-profile-content';
+
+// interface
+export interface Tab {
+  id: number
+  title: string
+}
+
+interface Props {}
+
+interface State {
+  tabs: Tab[]
+  isActiveTab: string
+}
+
+const tabs = [
+  {id: 1, title: 'Content'}, 
+  {id: 2, title: 'Groups'}, 
+  {id: 3, title: 'Schedule'}, 
+  {id: 4, title: 'Gifts'}, 
+  {id: 5, title: 'Charges'}, 
+  {id: 6, title: 'History'}, 
+  {id: 7, title: 'Settings'}
+];
+
+export default class MyProfile extends Component<Props, State> {
+  state = {
+    tabs: tabs,
+    isActiveTab: ''
+  };
+
+  tabClick = (tab: string) => {
+    this.setState({isActiveTab: tab});
+  };
+
+  render() {    
+    return (
+      <>
+        <MyProfileInfoPanel loginedEmail={'defaultEmail@gmail.com'}/>
+        <MyProfileTabNavigation tabs={this.state.tabs} isActiveTab={this.state.isActiveTab} tabClick={this.tabClick} />
+        {/* <MyProfileSettings/> */}
+        {/* <MyProfileContent itemArray={itemArray}/> */}
+        {/* <MyProfileHistory itemArray={itemArray} isHistoryComponent={true}/> */}
+      </>
+    )
+  }
+}
